fb80779d9b5113a62a3c1a639e9941d6a4b14405
MasterZer0o <kubapod1234@wp.pl>
2022-04-20 21:43:55 +0200

    added routes and guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,80 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
+import { username, isLoggedIn } from '../store/store';
+import { watch } from 'vue';
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
 			name: 'home',
-			component: HomeView
-		},
-		{
-			path: '/about',
-			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import('../views/AboutView.vue')
+			component: () => import('@/views/HomeView.vue')
 		},
 		{
 			path: '/login',
 			name: 'login',
-			component: () => import('../views/LoginView.vue')
+			component: () => import('../views/LoginView.vue'),
+			beforeEnter: (to, from) => {
+				// redirecting user to '/' if user came from other origin and is not logged in.
+				const unwatch = watch(username, () => {
+					// from address bar
+					if (from.name === undefined && username.value !== '') router.push('/');
+					return unwatch();
+				});
+			}
+		},
+		{
+			path: '/uczen',
+			name: 'uczen',
+			meta: { requiresAuth: true },
+			component: () => import('@/views/Student.vue')
+		},
+
+		{
+			path: '/uczen/oceny',
+			name: 'grades',
+			meta: { requiresAuth: true },
+			component: () => import('@/views/GradesView.vue')
+		},
+		{
+			path: '/uczen/planlekcji',
+			name: 'plan',
+			meta: { requiresAuth: true },
+			component: () => import('@/views/PlanView.vue')
+		},
+		{
+			path: '/uczen/frekwencja',
+			name: 'attendance',
+			meta: { requiresAuth: true },
+			component: () => import('@/views/FrekwencjaView.vue')
+		},
+		{
+			path: '/ustawienia',
+			name: 'userSettings',
+			meta: { requiresAuth: true },
+			component: () => import('@/views/SettingsView.vue')
+		},
+		{
+			path: '/:pathMatch(.*)*',
+			name: 'NotFound',
+			component: () => import('@/views/NotFoundView.vue')
 		}
 	]
+});
+
+router.beforeEach((to, from) => {
+	const unwatch = watch(isLoggedIn, () => {
+		// console.log(isLoggedIn.value);
+		// console.log(username.value);
+		// console.log(from);
+
+		if (to.meta.requiresAuth && !username.value) {
+			console.log('User is unauthenticated, redirecting to /');
+			router.push('/login');
+		} else {
+			console.log('All good');
+		}
+		unwatch();
+	});
 });
 
 export default router;
